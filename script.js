41fe68922d891a85d39bfdc16d228e1d111e9aff
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-10-29 22:23:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,14 @@ function append(value) {
   setTimeout(() => display.classList.remove('typing'), 200);
 }
 
-// Calculate result
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch {
-    display.value = 'Error';
-  }
-}
-
 // Clear display
 function clearDisplay() {
   display.value = '';
+}
+
+// Backspace (delete last character)
+function backspace() {
+  display.value = display.value.slice(0, -1);
 }
 
 // Theme toggle
